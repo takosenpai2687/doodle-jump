9f2d9f1890fd351ebcd42c7ad6f1aadced82e368
Jeremy9863 <kangtaohan9863@gmail.com>
2020-06-24 05:12:10 +1000

    docs folder for github pages

---
+++ docs/js/blackhole.js
@@ -0,0 +1,23 @@
+class Blackhole {
+  static blackholeImg;
+  static w = 40;
+  static h = 40;
+  // Minimum safe distance near blackhole
+  static ROCHE_LIMIT = Blackhole.h * 0.8;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+  }
+
+  render() {
+    image(
+      Blackhole.blackholeImg,
+      this.x - Blackhole.w / 2,
+      this.y - Blackhole.h / 2,
+      Blackhole.w,
+      Blackhole.h
+    );
+  }
+}
---
+++ docs/js/config.js
@@ -0,0 +1,19 @@
+/**
+ * Global config object
+ */
+const config = {
+  // Frames per second, this will affect game speed as well
+  FPS: 100,
+  // Gravity applied to doodler
+  GRAVITY: 0.05,
+  // Cell size for the background grid
+  CELL_SIZE: 30,
+  // Minimum value of the distance between Doodler and screen top
+  THRESHOLD: 300,
+  // Number of stairs per screen
+  STEPS: 8,
+  // Chance of springed platform spawn rate
+  SPRINGED_CHANCE: 0.1,
+  // Chance of blackhole
+  BLACKHOLE_CHANCE: 1
+};
